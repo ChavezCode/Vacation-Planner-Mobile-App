f8136c6756a67586a6cbb2147e3a33c3fac6b6dc
ChavezCode <jch1248@wgu.edu>
2025-01-08 18:25:22 -0800

    Report generating. Fix layout of textview items in report

--- app/src/main/java/com/example/vacationplanner/UI/Report.java
+++ app/src/main/java/com/example/vacationplanner/UI/Report.java
@@ -8,10 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vacationplanner.R;
+import com.example.vacationplanner.database.Repository;
+import com.example.vacationplanner.entities.Vacation;
+
+import java.util.List;
 
 public class Report extends AppCompatActivity {
+    private ReportAdapter reportAdapter;
     @Override
     protected void onCreate(Bundle savedInstance) {
 
@@ -23,5 +30,13 @@ public class Report extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Repository repository = new Repository(getApplication());
+        RecyclerView recyclerView=findViewById(R.id.recyclerReport);
+        List<Vacation> allVacations=repository.getmAllVacations();
+        reportAdapter=new ReportAdapter(this);
+        recyclerView.setAdapter(reportAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        reportAdapter.setReport(allVacations);
+
     }
 }
--- app/src/main/java/com/example/vacationplanner/UI/ReportAdapter.java
+++ app/src/main/java/com/example/vacationplanner/UI/ReportAdapter.java
@@ -1,15 +1,19 @@
 package com.example.vacationplanner.UI;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.vacationplanner.R;
 import com.example.vacationplanner.entities.Vacation;
 
+import java.util.Date;
 import java.util.List;
 
 public class ReportAdapter extends RecyclerView.Adapter<ReportAdapter.ReportViewHolder> {
@@ -22,27 +26,75 @@ public class ReportAdapter extends RecyclerView.Adapter<ReportAdapter.ReportView
         this.context=context;
     }
     public class ReportViewHolder extends RecyclerView.ViewHolder{
+        private final TextView reportName;
+        private final TextView reportStay;
+        private final TextView reportStart;
+        private final TextView reportEnd;
+
 
         public ReportViewHolder(@NonNull View itemView) {
             super(itemView);
             //get textviews from reportview
+
+            reportName = itemView.findViewById(R.id.reportViewName);
+            reportStay = itemView.findViewById(R.id.reportViewStay);
+            reportStart = itemView.findViewById(R.id.reportViewStartDate);
+            reportEnd = itemView.findViewById(R.id.reportViewEndDate);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position=getAdapterPosition();
+                    final Vacation current=allVacations.get(position);
+                    Intent intent = new Intent(context,Report.class);
+                    intent.putExtra("id", current.getVacationID());
+                    intent.putExtra("name", current.getVacationName());
+                    intent.putExtra("hotel", current.getHotel());
+                    intent.putExtra("startdate",current.getStartDate());
+                    intent.putExtra("enddate", current.getEndDate());
+                    //start activity
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 
     @NonNull
     @Override
     public ReportAdapter.ReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView=mInflater.inflate(R.layout.report_list_item, parent, false);
+        return new ReportViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ReportAdapter.ReportViewHolder holder, int position) {
+        if(allVacations!=null){
+            Vacation current=allVacations.get(position);
+            String name=current.getVacationName();
+            String stay=current.getHotel();
+            String start=current.getStartDate();
+            String end=current.getEndDate();
+            holder.reportName.setText(name);
+            holder.reportStay.setText(stay);
+            holder.reportStart.setText(start);
+            holder.reportEnd.setText(end);
+        }
+        else{
+            holder.reportName.setText("No Vacations");
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (allVacations!=null){
+            return allVacations.size();
+        }else {
+            return 0;
+        }
+    }
+    public void setReport(List<Vacation> vacations){
+        allVacations=vacations;
+        notifyDataSetChanged();
     }
 
 
